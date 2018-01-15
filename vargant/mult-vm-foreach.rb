c51789e4410b408ea89535dc2911dac489a70d7e
WZQ1397 <wzqsergeant@vip.qq.com>
2018-01-15 10:11:32 +0800

    Update mult-vm-foreach.rb

--- vargant/mult-vm-foreach.rb
+++ vargant/mult-vm-foreach.rb
@@ -1,25 +1,25 @@
 Vagrant.configure("2") do |config|
- (1..4).each do |i|
- config.ssh.username = "vagrant"
- config.ssh.password = "vagrant"
+   (1..4).each do |i|
+      config.ssh.username = "vagrant"
+      config.ssh.password = "vagrant"
 
- config.vm.define "node#{i}" do |node|
- # 设置虚拟机的Box
- node.vm.box = "ubuntu2"
- # 设置虚拟机的主机名
- node.vm.hostname="node#{i}"
- # 设置虚拟机的IP
- node.vm.network "private_network", ip: "192.168.33.#{i+3}"
- # 设置主机与虚拟机的共享目录
- node.vm.synced_folder "/share", "/home/vagrant/share"
- # VirtaulBox相关配置
- node.vm.provider "virtualbox" do |v|
- # 设置虚拟机的名称
- v.name = "node#{i}"
- # 设置虚拟机的内存大小
- v.memory = 2048
- # 设置虚拟机的CPU个数
- v.cpus = 1
- end
- end
- end
+      config.vm.define "node#{i}" do |node|
+         # 设置虚拟机的Box
+         node.vm.box = "ubuntu2"
+         # 设置虚拟机的主机名
+         node.vm.hostname="node#{i}"
+         # 设置虚拟机的IP
+         node.vm.network "private_network", ip: "192.168.33.#{i+3}"
+         # 设置主机与虚拟机的共享目录
+         node.vm.synced_folder "/share", "/home/vagrant/share"
+         # VirtaulBox相关配置
+         node.vm.provider "virtualbox" do |v|
+            # 设置虚拟机的名称
+            v.name = "node#{i}"
+            # 设置虚拟机的内存大小
+            v.memory = 2048
+            # 设置虚拟机的CPU个数
+            v.cpus = 1
+         end
+      end
+   end
